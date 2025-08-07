6fd08c1e8da72f93d865e1187fe200c1c440b2b8
Rafael Campos <zcampoz@gmail.com>
2025-08-07 11:22:06 -0300

    First Commit

---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi;
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options) 
+        : base(options) { }
+    public DbSet<TodoItem> Todos { get; set; } = default!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TodoItem>().HasKey(t => t.Id);
+        modelBuilder.Entity<TodoItem>().Property(t => t.Name).IsRequired().HasMaxLength(100);
+        modelBuilder.Entity<TodoItem>().Property(t => t.IsComplete).IsRequired();
+    }
+}
